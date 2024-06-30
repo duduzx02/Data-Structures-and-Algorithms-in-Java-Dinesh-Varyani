3f11e4564eadaf34e4b6bc056682270ebfa58607
Eduardo Lima <duduzx02@gmail.com>
2024-06-30 01:24:08 -0300

    Crie uma lista vinculada individualmente em Java (implementação)

---
+++ src/SinglyLinkedList/SinglyLinkedList.java
@@ -0,0 +1,31 @@
+package SinglyLinkedList;
+
+public class SinglyLinkedList {
+
+    private ListNode head;
+
+    private static class ListNode{
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+
+    public static void main(String[] args) {
+           SinglyLinkedList sll = new SinglyLinkedList();
+           sll.head = new ListNode(10);
+           ListNode second = new ListNode(1);
+           ListNode third = new ListNode(8);
+           ListNode fourth = new ListNode(11);
+
+           // Agora vamos conctar todos para fazer uma corrente.
+        sll.head.next = second; // 10 --> 1
+        second.next = third; // 10 --> 1 --> 8
+        third.next = fourth; // 10 --> 1 --> 8 --> 11 --> null
+
+    }
+}
